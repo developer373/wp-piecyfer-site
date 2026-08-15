f2d160a990193a72cbcb125a1374938ceaa9d1c0
PieCyfer Cleanup <umair@amaizing.io>
2026-08-15 15:07:05 +0500

    Phase C complete: removed nulled legacy packages (elementor-pro, elementskit, vamtam-elementor-integration, tecnologia theme, object-cache-pro, redis-cache, optinmonster)

--- wp-content/themes/tecnologia/vamtam/customizer/lib/class-vamtam-customizer-panel.php
+++
@@ -1,5 +0,0 @@
-<?php
-
-class Vamtam_Customizer_Panel extends WP_Customize_Panel {
-	public $type = 'vamtam';
-}
--- wp-content/themes/tecnologia/vamtam/customizer/lib/class-vamtam-customizer-section.php
+++
@@ -1,5 +0,0 @@
-<?php
-
-class Vamtam_Customizer_Section extends WP_Customize_Section {
-	public $type = 'vamtam';
-}
