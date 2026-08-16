cda0bcc036932a7140c7bf6ebcb06a71440238ef
PieCyfer Cleanup <umair@amaizing.io>
2026-08-16 17:47:46 +0500

    fix(theme): add nav menu theme support and add ThemeBuilder repair script

--- wp-content/themes/piecyfer-theme/inc/setup.php
+++ wp-content/themes/piecyfer-theme/inc/setup.php
@@ -54,6 +54,13 @@ const PIECYFER_ELEMENTOR_FEATURES = array(
 function piecyfer_setup() {
 	load_theme_textdomain( 'piecyfer-theme', PIECYFER_THEME_DIR . 'languages' );
 
+	add_theme_support( 'menus' );
+	register_nav_menus(
+		array(
+			'primary-menu' => esc_html__( 'Primary Menu', 'piecyfer-theme' ),
+		)
+	);
+
 	add_theme_support( 'post-thumbnails' );
 	add_theme_support( 'automatic-feed-links' );
 	add_theme_support( 'html5', array( 'comment-list', 'comment-form', 'search-form', 'gallery', 'caption' ) );
