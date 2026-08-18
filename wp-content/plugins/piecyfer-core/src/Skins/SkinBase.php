d04bd130895f85b90bbbc4e253e7ac1dcb0af1e5
PieCyfer Cleanup <umair@amaizing.io>
2026-08-18 18:41:29 +0500

    fix(accessibility): resolve Lighthouse 'Links do not have descriptive text' on blog read more links and add screen-reader utility classes

--- wp-content/plugins/piecyfer-core/src/Skins/SkinBase.php
+++ wp-content/plugins/piecyfer-core/src/Skins/SkinBase.php
@@ -1159,8 +1159,9 @@ abstract class SkinBase extends Elementor_Skin_Base {
 			<div class="elementor-post__read-more-wrapper">
 		<?php endif; ?>
 
-		<a class="elementor-post__read-more" href="<?php echo esc_url( $this->current_permalink ); ?>" aria-label="<?php echo esc_attr( $aria_label_text ); ?>" tabindex="-1" <?php Utils::print_unescaped_internal_string( $optional_attributes_html ); ?>>
+		<a class="elementor-post__read-more" href="<?php echo esc_url( $this->current_permalink ); ?>" aria-label="<?php echo esc_attr( $aria_label_text ); ?>" <?php Utils::print_unescaped_internal_string( $optional_attributes_html ); ?>>
 			<?php echo wp_kses_post( $read_more ); ?>
+			<span class="elementor-screen-only"><?php echo esc_html( ': ' . get_the_title() ); ?></span>
 		</a>
 
 		<?php if ( $this->display_read_more_bottom() ) : ?>
